e48bbada3be094442a5e4649eb87565d0bf34e0d
Julaczos <julaczosoo@gmail.com>
2025-03-16 18:25:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function showPage(pageId) {
-    document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
-    document.getElementById(pageId).classList.add('active');
+    // Usuwamy klasę 'active' z wszystkich sekcji
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.classList.remove('active');
+    });
+
+    // Dodajemy klasę 'active' do wybranej sekcji
+    const activePage = document.getElementById(pageId);
+    activePage.classList.add('active');
 }
